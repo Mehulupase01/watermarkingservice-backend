8cc2c0e8f979d23bb2fa69d914b9e2a1977445bb
Nimish Pandey <pandeynimish2001@gmail.com>
2024-05-31 10:15:52 +0200

    a few changes in CORS

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const watermarkRoutes = require("./routes/watermark");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  // Update this line to allow your web app's origin
+  res.setHeader('Access-Control-Allow-Origin', 'http://watermark.fron.web.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Max-Age', '86400');
+    res.sendStatus(204);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use('/api/watermark', watermarkRoutes);
 
