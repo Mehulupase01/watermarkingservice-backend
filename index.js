a73dda0a8711a220585439830d3607a0028bc936
Mehul Bhojraj Upase <mehulupase@gmail.com>
2024-08-01 09:52:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const cors = require("cors");
 
 const app = express();
 
+// Set up CORS
 app.use(cors({
   origin: function (origin, callback) {
-    const allowedOrigins = ['https://watermark-fron.web.app/', 'https://ass2vid1.storage.googleapis.com'];
+    const allowedOrigins = ['https://watermark-fron.web.app', 'https://ass2vid1.storage.googleapis.com'];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
@@ -24,4 +25,13 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/watermark', watermarkRoutes);
 
+// Local development server
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+// Export for serverless deployment
 module.exports = { app: serverless(app) };
